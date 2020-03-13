28c418073590a7b70524d3c4688de287d05267a8
AlexVipond <alex.r.vipond@gmail.com>
2020-03-12 18:59:10 -0600

    upgrade ava

--- ava.config.js
+++ ava.config.js
@@ -1,11 +1,16 @@
 export default {
   files: [ 'tests/**/*.test.js' ],
-  helpers: ['src/**/*.js', 'tests/test-util/*.js', 'tests/fixtures/*.js', 'node_modules/@baleada/prose/vue/propsInterfaces.js'],
   verbose: true,
   require: [
     './tests/test-util/setup.js'
   ],
   babel: {
+    compileAsTests: [
+      'src/**/*.js',
+      'tests/test-util/*.js',
+      'tests/fixtures/*.js',
+      'node_modules/@baleada/prose/vue/propsInterfaces.js'
+    ],
     testOptions: {
       presets: [
         [
